4b1788462486d8bb8da88fc7e276e93898149c48
eric.roys@gmail.com <eric.roys@gmail.com>
2024-09-17 16:57:27 -0500

    feature: add building blocks for create/query company pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import { ApolloProvider } from '@apollo/client';
 import { client } from './config/apollo';
-import { T } from './t'
+// import { T } from './t'
 import React from 'react';
+// import { CreateCompany } from './components/createCompany';
+import { Company } from './components/company';
 
 const App: React.FC = () => {
   return (
     <ApolloProvider client={client}>
-      <T />
+      <Company />
     </ApolloProvider>
   );
 };
---
+++ client/src/components/companyItem.tsx
@@ -0,0 +1,3 @@
+export const CompanyItem = () => {
+  return <></>;
+};
